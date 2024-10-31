2d2a0da2944f04d8973444175869c65e4fc5ccc5
Alexis Hamon <alexis.hamon@ens-rennes.fr>
2024-10-31 18:30:45 +0000

    ✨ compressed rail view

--- src/display_track.cpp
+++ src/display_track.cpp
@@ -14,32 +14,38 @@ ArcTrack::ArcTrack(Vector center, float radius, float gauge, float angle_min, fl
 
 void ArcTrack::draw(Layout layout) {
 	auto display_gauge = gauge * (layout.size.x / GAUGE_FACTOR);
-   
     int delta = layout.size.x / 20;
-    for (float i=0; i<=30; ++i) {
-        float angle = (i / 30) * (angle_max - angle_min) + angle_min;
-        Vector v1 = Vector{center.x + cosf(DEG2RAD*angle)*(radius+display_gauge/2+delta), center.y + sinf(DEG2RAD*angle)*(radius+display_gauge/2+delta)};
-        Vector v2 = Vector{center.x + cosf(DEG2RAD*angle)*(radius-display_gauge/2-delta), center.y + sinf(DEG2RAD*angle)*(radius-display_gauge/2-delta)};
-        DrawLineEx(v1.to_Vector2(), v2.to_Vector2(), 2, BROWN);
+
+    if (layout.size.x > 40) {
+        for (float i=0; i<=30; ++i) {
+            float angle = (i / 30) * (angle_max - angle_min) + angle_min;
+            Vector v1 = Vector{center.x + cosf(DEG2RAD*angle)*(radius+display_gauge/2+delta), center.y + sinf(DEG2RAD*angle)*(radius+display_gauge/2+delta)};
+            Vector v2 = Vector{center.x + cosf(DEG2RAD*angle)*(radius-display_gauge/2-delta), center.y + sinf(DEG2RAD*angle)*(radius-display_gauge/2-delta)};
+            DrawLineEx(v1.to_Vector2(), v2.to_Vector2(), 2, BROWN);
+        }
+    } else {
+        DrawRing(center.to_Vector2(), radius-display_gauge/2-delta, radius+display_gauge/2+delta, angle_min, angle_max, 30, BROWN);
     }
-
     DrawCircleSectorLines(center.to_Vector2(), radius+display_gauge/2, angle_min, angle_max, DISCRETISATION, BLACK);
-	DrawCircleSectorLines(center.to_Vector2(), radius-display_gauge/2, angle_min, angle_max, DISCRETISATION, BLACK);
-
+    DrawCircleSectorLines(center.to_Vector2(), radius-display_gauge/2, angle_min, angle_max, DISCRETISATION, BLACK);
 }
 
 StraighTrack::StraighTrack(Vector src, Vector dst, float gauge)
     : src{src}, dst{dst}, gauge{gauge} {};
 
 void StraighTrack::draw(Layout layout) {
-	auto display_gauge = gauge * (layout.size.x / GAUGE_FACTOR);
-    Vector ortho = (dst-src).orthogonal().normalise();
+    auto display_gauge = gauge * (layout.size.x / GAUGE_FACTOR);
     int delta = layout.size.x / 20;
-    for (float i=0; i<=30; ++i) {
-        Vector v = (dst - src) * (i / 30) + src; 
-        DrawLineEx((v + ortho * (display_gauge / 2 + delta)).to_Vector2(), (v - ortho * (display_gauge / 2 + delta)).to_Vector2(), 2, BROWN);
+    Vector ortho = (dst-src).orthogonal().normalise();
+    if (layout.size.x > 40) {
+        for (float i=0; i<=30; ++i) {
+            Vector v = (dst - src) * (i / 30) + src; 
+            DrawLineEx((v + ortho * (display_gauge / 2 + delta)).to_Vector2(), (v - ortho * (display_gauge / 2 + delta)).to_Vector2(), 2, BROWN);
+        }
+    } else {
+        DrawLineEx(src.to_Vector2(), dst.to_Vector2(), display_gauge+2*delta, BROWN);
     }
-
     DrawLineV((src+ortho*display_gauge/2).to_Vector2(), (dst+ortho*display_gauge/2).to_Vector2(), BLACK);
     DrawLineV((src-ortho*display_gauge/2).to_Vector2(), (dst-ortho*display_gauge/2).to_Vector2(), BLACK);
+   
 }
