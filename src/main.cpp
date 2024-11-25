70cb19e288c525eaed23cab92100b64fd5847196
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-11-25 07:48:19 +0100

    🚨 remove unused_result warning from release compile of raygui

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-result"
 #pragma GCC diagnostic ignored "-Wenum-compare"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
