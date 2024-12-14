12b8021b528ee7761c61121342118e5191412c12
Alexis Hamon <alexis.hamon@ens-rennes.fr>
2024-12-14 01:49:45 +0000

    🥅 add size checking for cyclic buffer

--- src/train/cyclic_track_buffer.cpp
+++ src/train/cyclic_track_buffer.cpp
@@ -1,14 +1,17 @@
 #include "cyclic_track_buffer.hpp"
 
+#include <assert.h>
+
 int mod(int a, int b) {
     return ((a % b) + b) % b;
 }
 
-Cyclic_buffer::Cyclic_buffer(std::size_t size) {
-    _size = size;
+Cyclic_buffer::Cyclic_buffer(std::size_t sizemax) {
+    _sizemax = sizemax;
+    _size = 0;
     _index = 0;
     _incr = 1;
-    _buffer = (struct prev_rail *)calloc(size, sizeof(struct prev_rail));
+    _buffer = (prev_rail_s*) calloc(sizemax, sizeof(prev_rail_s));
     
 }
 
@@ -16,17 +19,31 @@ Cyclic_buffer::~Cyclic_buffer() {
     free(_buffer);
 }
 
+std::size_t Cyclic_buffer::index_min() {
+    return _index - ((_size-1)*_incr);
+}
+
 void Cyclic_buffer::add_prev_rail(int rail_id, int direction) {
+    assert(_size < _sizemax);
     _index = (_index + _incr) % _size;
     _buffer[_index].rail_id = rail_id;
     _buffer[_index].direction = direction;
+    _size++;
 }
 
-prev_rail Cyclic_buffer::get_prev_rail(int n) {
+prev_rail_s Cyclic_buffer::del_last_prev_rail() {
+    assert(_size > 0);
+    prev_rail_s ret = get_prev_rail(_size-1);
+    _size--;
+    return ret;
+}
+
+prev_rail_s Cyclic_buffer::get_prev_rail(int n) {
+    assert((0 <= n) && (n < _size));
     return _buffer[mod(_index-(n*_incr), _size)];
 }
 
 void Cyclic_buffer::reverse() {
-    _index = _index - ((_size-1)*_incr);
+    _index = this->index_min();
     _incr = _incr * -1;
 }
--- src/train/cyclic_track_buffer.hpp
+++ src/train/cyclic_track_buffer.hpp
@@ -2,7 +2,7 @@
 
 #include <stdlib.h>
 
-struct prev_rail {
+struct prev_rail_s {
 	int rail_id;
 	int direction;
 };
@@ -10,16 +10,21 @@ struct prev_rail {
 class Cyclic_buffer {
     protected:
         std::size_t _size;
+        std::size_t _sizemax;
         std::size_t _index;
         int _incr;
-        prev_rail* _buffer;
+        prev_rail_s* _buffer;
+
+    private:
+        std::size_t index_min();
 
     public:
         Cyclic_buffer(std::size_t size);
         ~Cyclic_buffer();
 
         void add_prev_rail(int rail_id, int direction);
-        struct prev_rail get_prev_rail(int n);
+        prev_rail_s Cyclic_buffer::del_last_prev_rail();
+        prev_rail_s get_prev_rail(int n);
 
         void reverse();
 };
