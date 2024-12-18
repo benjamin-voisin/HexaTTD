e51be9521e506b79536eadf57bbe768d21ce51c5
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-12-18 14:18:40 +0100

    🐛 initialize GuiToggleElement

--- src/gui/toggle.cpp
+++ src/gui/toggle.cpp
@@ -1,13 +1,12 @@
 #include "toggle.hpp"
 #include "gui.hpp"
 
-GuiToggleElement::GuiToggleElement(float x, float y, float width, float height, std::string text)
-	: GuiElement(x, y, width, height), _text{text} { }
+GuiToggleElement::GuiToggleElement(float x, float y, float width, float height,
+                                   std::string text, bool init_value)
+    : GuiElement(x, y, width, height), _pressed{init_value}, _text{text} {}
 
 void GuiToggleElement::draw() {
-	GuiToggle((Rectangle){_x, _y, _width, _height}, _text.c_str(), &_pressed);
+    GuiToggle((Rectangle){_x, _y, _width, _height}, _text.c_str(), &_pressed);
 }
 
-bool GuiToggleElement::is_pressed() {
-	return _pressed;
-}
+bool GuiToggleElement::is_pressed() { return _pressed; }
--- src/gui/toggle.hpp
+++ src/gui/toggle.hpp
@@ -4,12 +4,13 @@
 #include <string>
 
 class GuiToggleElement : GuiElement {
-	private:
-		bool _pressed;
-		std::string _text;
+  private:
+    bool _pressed;
+    std::string _text;
 
-	public:
-		void draw() override;
-		bool is_pressed();
-		GuiToggleElement(float x, float y, float width, float height, std::string text);
+  public:
+    void draw() override;
+    bool is_pressed();
+    GuiToggleElement(float x, float y, float width, float height,
+                     std::string text, bool init_value);
 };
