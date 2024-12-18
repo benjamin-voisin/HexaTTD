d30e967ca1c06bf2819214e9f5885d4928d2cc94
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-12-18 14:24:48 +0100

    ⚰️ remove commented code moved to game.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,5 @@
 #include "game.hpp"
 
-/* const char dotfile[20] = "graph.dot"; */
-
-/* void pp_int_rail_vector(Grid *g, FILE *f, std::vector<int> v) { */
-/*     fprintf(f, "["); */
-/*     for (long unsigned i = 0; i < v.size(); ++i) { */
-/*         if (i > 0) */
-/*             fprintf(f, ", "); */
-/*         g->get_rail(v[i])->pp(f); */
-/*     } */
-/*     fprintf(f, "]\n"); */
-/* }; */
-
 int main() {
     Game *game = new Game(1000, 1000, "HexaTTD");
     game->start();
