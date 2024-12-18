86577b2bea7ab2aca64e1a7aa325efb54053231a
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-12-18 14:22:03 +0100

    ✨ add back debug toggle to game

--- src/game.cpp
+++ src/game.cpp
@@ -1,18 +1,20 @@
 #include "game.hpp"
 #include "raylib.h"
 
-void update(Grid *grid) {
-    Hex last_cursor = grid->xy_to_hex(GetMouseX(), GetMouseY());
-    Hex last_cursor_pers = grid->xy_to_hex(GetMouseX(), GetMouseY());
+#include <iostream>
 
-    Hex start_construct = grid->xy_to_hex(GetMouseX(), GetMouseY());
+void Game::update() {
+    Hex last_cursor = _grid.xy_to_hex(GetMouseX(), GetMouseY());
+    Hex last_cursor_pers = _grid.xy_to_hex(GetMouseX(), GetMouseY());
+
+    Hex start_construct = _grid.xy_to_hex(GetMouseX(), GetMouseY());
 
     auto target = std::chrono::milliseconds(16);
     auto target_fast = std::chrono::milliseconds(1);
 
-    while (grid->is_running()) {
-        grid->update();
-        Hex under_cursor = grid->xy_to_hex(GetMouseX(), GetMouseY());
+    while (_grid.is_running()) {
+        _grid.update();
+        Hex under_cursor = _grid.xy_to_hex(GetMouseX(), GetMouseY());
 
         if (under_cursor != last_cursor)
             last_cursor_pers = last_cursor;
@@ -30,7 +32,7 @@ void update(Grid *grid) {
                     Hex diff_dst = under_cursor - last_cursor_pers;
                     if ((diff_src.direction() != diff_dst.direction()) &&
                         !(start_construct.is_neighbor(under_cursor))) {
-                        grid->add_rail(last_cursor_pers, diff_src.direction(),
+                        _grid.add_rail(last_cursor_pers, diff_src.direction(),
                                        diff_dst.direction(), 4);
                         start_construct = last_cursor_pers;
                     }
@@ -47,66 +49,78 @@ void update(Grid *grid) {
     }
 }
 
-void draw(Grid *grid) {
+void pp_int_rail_vector(Grid *g, FILE *f, std::vector<int> v) {
+    fprintf(f, "[");
+    for (long unsigned i = 0; i < v.size(); ++i) {
+        if (i > 0)
+            fprintf(f, ", ");
+        g->get_rail(v[i])->pp(f);
+    }
+    fprintf(f, "]\n");
+};
+
+void Game::draw() {
+
+    char *texte = (char *)calloc(1000, sizeof(char));
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(DARKGREEN);
         // Move the map
         if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
             Vector2 delta = GetMouseDelta();
-            grid->layout.origin.x += delta.x;
-            grid->layout.origin.y += delta.y;
+            _grid.layout.origin.x += delta.x;
+            _grid.layout.origin.y += delta.y;
         }
         // Update the zoom level
-        grid->update_zoom(WHEEL_FACTOR, true);
+        _grid.update_zoom(WHEEL_FACTOR, true);
 
         // Draw the main thing
-        grid->draw();
+        _grid.draw();
 
         // Draw hightlighted hex
-        Hex under_cursor = grid->xy_to_hex(GetMouseX(), GetMouseY());
+        Hex under_cursor = _grid.xy_to_hex(GetMouseX(), GetMouseY());
 
         // Draw hightlighted rails
         Vector pos = {(float)GetMouseX(), (float)GetMouseY()};
-        if (grid->on_grid(under_cursor)) {
-            Tile *t = grid->tile_from_hex(under_cursor);
+        if (_grid.on_grid(under_cursor)) {
+            Tile *t = _grid.tile_from_hex(under_cursor);
             std::set<int> on_tile_tracks = t->get_rails_on_tile();
             std::vector<int> selected_rails = {};
             for (auto n = on_tile_tracks.begin(); n != on_tile_tracks.end();
                  ++n) {
-                Rail *r = grid->get_rail(*n);
-                if (r->is_on_track(grid->layout, pos)) {
+                Rail *r = _grid.get_rail(*n);
+                if (r->is_on_track(_grid.layout, pos)) {
                     selected_rails.push_back(*n);
                 }
             }
             if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
                 for (long unsigned i = 0; i < selected_rails.size(); ++i) {
-                    grid->del_rail(selected_rails[i]);
+                    _grid.del_rail(selected_rails[i]);
                 }
             } else {
                 for (long unsigned i = 0; i < selected_rails.size(); ++i) {
-                    Rail *r = grid->get_rail(selected_rails[i]);
+                    Rail *r = _grid.get_rail(selected_rails[i]);
                     if (!r->deleted)
-                        r->draw(grid->layout, ORANGE, 1);
+                        r->draw(_grid.layout, ORANGE, 1);
                 }
             }
-            /* if (debug_toggle.is_pressed()) { */
-            /*     // Print selected rails on debug mode */
-            /*     FILE *f = fmemopen(texte, 1000, "w"); */
-            /*     fprintf(f, "selected_rails= "); */
-            /*     pp_int_rail_vector(&grid, f, selected_rails); */
-            /*     fprintf(f, "layout.x=%.2f\n", grid.layout.size.x); */
-            /*     t->pp(f); */
-            /*     fclose(f); */
-            /*     DrawText(texte, 10, 40, 30, BLACK); */
-            /* } */
+            if (_debug_toggle.is_pressed()) {
+                // Print selected rails on debug mode
+                FILE *f = fmemopen(texte, 1000, "w");
+                fprintf(f, "selected_rails= ");
+                pp_int_rail_vector(&_grid, f, selected_rails);
+                fprintf(f, "layout.x=%.2f\n", _grid.layout.size.x);
+                t->pp(f);
+                fclose(f);
+                DrawText(texte, 10, 40, 30, BLACK);
+            }
         }
 
-        grid->hightlight(under_cursor, GREEN);
+        _grid.hightlight(under_cursor, GREEN);
 
         DrawFPS(10, 10);
 
-        /* debug_toggle.draw(); */
+        _debug_toggle.draw();
         /* if (debug_toggle.is_pressed()) { */
         /* // Hightlight */
         /* grid.hightlight(last_cursor_pers, BLUE); */
@@ -115,13 +129,15 @@ void draw(Grid *grid) {
 
         EndDrawing();
     }
+    free(texte);
 }
 
 Game::Game(int width, int height, std::string name)
     : _grid{Grid(layout_flat, Vector2{100, 100},
                  Vector2{(float)width / 2, (float)height / 2}, -10, 10, -10,
                  10)},
-      _name{name} {
+      _name{name},
+      _debug_toggle{GuiToggleElement(width - 200, 10, 80, 20, "debug", false)} {
     _grid.add_rail(Hex(0, 0), 1, 5, 5);
     _grid.add_rail(Hex(1, -1), 2, 5, 5);
     _grid.add_rail(Hex(1, -1) + Hex(1, -1), 2, 0, 5);
@@ -132,11 +148,11 @@ Game::Game(int width, int height, std::string name)
 }
 
 void Game::start() {
-    _update_thread = std::thread([this]() { update(&_grid); });
+    _update_thread = std::thread([this]() { update(); });
     _draw_thread = std::thread([this]() {
         InitWindow(1000, 1000, this->_name.c_str());
         SetTargetFPS(60);
-        draw(&_grid);
+        draw();
         this->_grid.stop();
     });
 }
--- src/game.hpp
+++ src/game.hpp
@@ -1,18 +1,22 @@
 #pragma once
 
 #include "grid.hpp"
+#include "gui/toggle.hpp"
 
 #include <thread>
 
-class Game {
+struct Game {
   private:
     Grid _grid;
     std::thread _update_thread;
     std::thread _draw_thread;
     bool _running;
     std::string _name;
+    GuiToggleElement _debug_toggle;
 
   public:
+    void update();
+    void draw();
     void start();
     Game(int width, int height, std::string name);
     void wait();
