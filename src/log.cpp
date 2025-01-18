a8bd31ec0f816171856c5044e4777bb2c1c656b1
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2025-01-18 18:43:46 +0100

    💄 add colors to logs

--- src/log.cpp
+++ src/log.cpp
@@ -1,5 +1,6 @@
 #include "log.hpp"
 
+#include <assert.h>
 #include <cstdio>
 #include <iostream>
 
@@ -20,15 +21,15 @@ Logger Log::Error = Logger(Log::Level_Error);
 std::string level_to_string(Log::loglevel level) {
     switch (level) {
     case Log::Level_Debug:
-        return "[DEBUG]";
+        return Log::ansi_color_cyan + "[DEBUG]" + Log::ansi_reset;
     case Log::Level_Info:
-        return "[INFO]";
+        return Log::ansi_color_green + "[INFO]" + Log::ansi_reset;
     case Log::Level_Fatal:
-        return "[FATAL]";
+        return Log::ansi_color_red + "[FATAL]" + Log::ansi_reset;
     case Log::Level_Warning:
-        return "[WARNING]";
+        return Log::ansi_color_yellow + "[WARNING]" + Log::ansi_reset;
     case Log::Level_Error:
-        return "[ERROR]";
+        return Log::ansi_color_red + "[ERROR]" + Log::ansi_reset;
     default:
         return "[?]";
     }
@@ -46,35 +47,40 @@ Logger &Logger::operator<<(const char *value) {
 
 void Logger::log(const char *format, ...) {
     va_list args;
-    char timeStr[64] = {0};
+    char timeStr[11] = {0};
     time_t now = time(NULL);
     struct tm *tm_info = localtime(&now);
-    strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", tm_info);
+    strftime(timeStr, sizeof(timeStr), "[%H:%M:%S]", tm_info);
 
     char text[100];
     snprintf(text, 100, format, args);
 
-    std::cout << "[" << timeStr << "][" << _text << "] " << text << std::endl;
+    std::cout << timeStr << _text << " " << text << std::endl;
 }
 
 void raylib_log(int msgType, const char *text, va_list args) {
 
-    vprintf(text, args);
+    /* vprintf(text, args); */
 
     switch (msgType) {
     case LOG_INFO:
-        Log::Info << text;
+        /* Log::Info << text; */
+        Log::Info.log(text, args);
         break;
     case LOG_ERROR:
-        Log::Error << text;
+        /* Log::Error << text; */
+        Log::Error.log(text, args);
         break;
     case LOG_WARNING:
-        Log::Warning << text;
+        /* Log::Warning << text; */
+        Log::Warning.log(text, args);
         break;
     case LOG_DEBUG:
-        Log::Debug << text;
+        /* Log::Debug << text; */
+        Log::Debug.log(text, args);
         break;
     default:
+        assert(false);
         break;
     }
 }
--- src/log.hpp
+++ src/log.hpp
@@ -5,6 +5,15 @@
 #include <string>
 
 namespace Log {
+const std::string ansi_reset = "\033[0m";
+const std::string ansi_color_red = "\033[31m";
+const std::string ansi_color_red_bold = "\033[1;31m";
+const std::string ansi_color_green = "\033[32m";
+const std::string ansi_color_yellow = "\033[33m";
+const std::string ansi_color_magenta = "\033[35m";
+const std::string ansi_color_magenta_bold = "\033[1;35m";
+const std::string ansi_color_cyan = "\033[36m";
+const std::string ansi_color_light_gray = "\033[37m";
 enum loglevel {
     Level_Info = LOG_INFO,
     Level_Warning = LOG_WARNING,
