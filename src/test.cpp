464ffc0650825541a68a83a4a11ec9469e7bea55
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2025-01-20 14:22:30 +0100

    ✅ update test as hex.hpp moved

--- src/test.cpp
+++ src/test.cpp
@@ -4,41 +4,39 @@
 #include "test.hpp"
 
 void Test::run_tests() {
-	for (size_t i = 0; i < _tests.size(); i++) {
-		_tests[i]->run_test();
-	}
+    for (size_t i = 0; i < _tests.size(); i++) {
+        _tests[i]->run_test();
+    }
 }
 
 void Test::add_test(std::shared_ptr<TestInstance> test) {
-	_tests.push_back(test);
+    _tests.push_back(test);
 }
 
-void TestInstance::run_test() {
-	std::cout << "Testing " << _name << "\n";
-}
+void TestInstance::run_test() { std::cout << "Testing " << _name << "\n"; }
 
 TestInstance::TestInstance(std::string name) : _name{name} {
-	_total_tests = 0;
-	_passed_tests = 0;
+    _total_tests = 0;
+    _passed_tests = 0;
 }
 
 void TestInstance::test(bool condition, std::string name) {
-	_total_tests++;
-	if (condition) {
-		_passed_tests++;
-		std::cout << name << Test::green << " \tPASSED\n" << Test::def;
-	} else {
-		std::cout << name << Test::red << " \tFAILED\n" << Test::def;
-	}
+    _total_tests++;
+    if (condition) {
+        _passed_tests++;
+        std::cout << name << Test::green << " \tPASSED\n" << Test::def;
+    } else {
+        std::cout << name << Test::red << " \tFAILED\n" << Test::def;
+    }
 }
 
 #ifdef TEST
 
-#include "hex.hpp"
+#include "geometry/hex.hpp"
 
 int main() {
-	Test test;
-	test.add_test(std::make_shared<HexTest>(HexTest()));
-	test.run_tests();
+    Test test;
+    test.add_test(std::make_shared<HexTest>(HexTest()));
+    test.run_tests();
 }
 #endif // TEST
