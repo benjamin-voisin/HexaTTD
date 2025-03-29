f603b16172da446714a6fd7822cdf6be75217289
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2025-03-29 14:46:42 +0100

    🔥 remove raygui related code

--- src/gui/gui.hpp
+++ src/gui/gui.hpp
@@ -6,17 +6,16 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-result"
 #pragma GCC diagnostic ignored "-Wenum-compare"
-#include "raygui.h"
 #pragma GCC diagnostic pop
 
-
 class GuiElement {
-	protected:
-		float _x;
-		float _y;
-		float _width;
-		float _height;
-	public:
-		GuiElement(float x, float y, float width, float height);
-		virtual void draw();
+  protected:
+    float _x;
+    float _y;
+    float _width;
+    float _height;
+
+  public:
+    GuiElement(float x, float y, float width, float height);
+    virtual void draw();
 };
--- src/gui/toggle.cpp
+++ src/gui/toggle.cpp
@@ -5,8 +5,6 @@ GuiToggleElement::GuiToggleElement(float x, float y, float width, float height,
                                    std::string text, bool init_value)
     : GuiElement(x, y, width, height), _pressed{init_value}, _text{text} {}
 
-void GuiToggleElement::draw() {
-    GuiToggle((Rectangle){_x, _y, _width, _height}, _text.c_str(), &_pressed);
-}
+void GuiToggleElement::draw() {}
 
 bool GuiToggleElement::is_pressed() { return _pressed; }
