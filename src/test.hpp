d345229137b8610bf3161c0fb053c867376d9963
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2025-04-01 23:21:35 +0200

    ✅ add som logic to tests

--- src/test.hpp
+++ src/test.hpp
@@ -5,28 +5,31 @@
 #include <vector>
 
 class TestCase {
-	private:
+  private:
 };
 
 class TestInstance {
-	protected:
-		int _total_tests;
-		int _passed_tests;
-		std::string _name;
-		void test(bool condition, std::string name);
-	public:
-		virtual void run_test();
-		TestInstance(std::string name);
+  protected:
+    int _total_tests;
+    int _passed_tests;
+    std::string _name;
+    void test(bool condition, std::string name);
+    void print_result();
+
+  public:
+    virtual void run_test();
+    TestInstance(std::string name);
 };
 
 class Test {
-	private:
-		std::vector<std::shared_ptr<TestInstance>> _tests;
-	public:
-		void add_test(std::shared_ptr<TestInstance>);
-		void run_tests();
-		static constexpr std::string green = "\033[032m";
-		static constexpr std::string orange = "\033[033m";
-		static constexpr std::string red = "\033[031m";
-		static constexpr std::string def = "\033[0m";
+  private:
+    std::vector<std::shared_ptr<TestInstance>> _tests;
+
+  public:
+    void add_test(std::shared_ptr<TestInstance>);
+    void run_tests();
+    static constexpr std::string green = "\033[032m";
+    static constexpr std::string orange = "\033[033m";
+    static constexpr std::string red = "\033[031m";
+    static constexpr std::string def = "\033[0m";
 };
