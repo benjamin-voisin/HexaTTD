5c2c7c39da1becea436ac4f5eca4be8e98b0bb03
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2025-04-09 17:42:02 +0200

    ⚡️ do not draw rails if out of view

--- src/rail.cpp
+++ src/rail.cpp
@@ -112,30 +112,14 @@ bool Rail::is_on_track(Layout *layout, Vector v) {
 }
 
 void Rail::draw(Layout *layout, Color c, int phase) {
+    // On ne dessine pas le rail s’il sort de l’affichage
+    if (!_hex.is_visible(layout))
+        return;
     // Vector center = Vector(_hex.center(layout));
     std::vector<Vector2> corners = _hex.corners(layout);
 
     if (this->_on_track_train.size() > 0)
         c = GREEN;
-
-    // Hex tile_src = _hex.neighbor(_src_neighbor);
-    // Hex tile_dst = _hex.neighbor(_dst_neighbor);
-
-    /* DEBUG STRUCT RAILS */
-    /*
-    DrawLineEx(_hex.center_side(layout, _src_neighbor), _hex.center(layout),
-    _width, GREEN); DrawLineEx(_hex.center(layout), _hex.center_side(layout,
-    _dst_neighbor), _width, PURPLE);
-
-    DrawCircleV(_hex.center_side(layout, _src_neighbor), 6, PURPLE);
-    DrawCircleV(_hex.corner(layout, _src_neighbor), 6, BLUE);
-    DrawCircleV(_hex.corner(layout, _src_neighbor+1), 6, BLUE);
-
-
-    DrawCircleV(_hex.center_side(layout, _dst_neighbor), 6, GREEN);
-    DrawCircleV(_hex.corner(layout, _dst_neighbor), 6, YELLOW);
-    DrawCircleV(_hex.corner(layout, _dst_neighbor+1), 6, YELLOW);
-    */
 
     if (_src_neighbor + 3 == _dst_neighbor) {
         // Si l'on doit faire une ligne droite
@@ -143,10 +127,6 @@ void Rail::draw(Layout *layout, Color c, int phase) {
             StraighTrack(c, _hex.center_side(layout, _src_neighbor),
                          _hex.center_side(layout, _dst_neighbor), GAUGE);
         track.draw(layout, phase);
-        // DrawLineEx(coffset_droite_src.to_Vector2(),
-        // coffset_droite_dst.to_Vector2(), _width, BLACK);
-        // DrawLineEx(coffset_gauche_src.to_Vector2(),
-        // coffset_gauche_dst.to_Vector2(), _width, BLACK);
     } else {
         if (_dst_neighbor == _src_neighbor + 2) {
             Hex tile_curb = _hex.neighbor(_src_neighbor + 1);
@@ -174,98 +154,6 @@ void Rail::draw(Layout *layout, Color c, int phase) {
             track.draw(layout, phase);
         }
     }
-    /*
-    Vector c1 = Vector(corners[_entry_neighbor]);
-    Vector c2 = Vector(corners[_entry_neighbor + 1]);
-    Vector c3 = Vector(corners[_exit_neighbor]);
-    Vector c4 = Vector(corners[_exit_neighbor + 1]);
-
-    Vector coffset_center_src = Vector(GetSplinePointLinear(c1.to_Vector2(),
-    c2.to_Vector2(), 0.50)); Vector coffset_droite_src =
-    Vector(GetSplinePointLinear(c1.to_Vector2(), c2.to_Vector2(), 0.25)); Vector
-    coffset_gauche_src = Vector(GetSplinePointLinear(c1.to_Vector2(),
-    c2.to_Vector2(), 0.75));
-
-    Vector coffset_center_dst = Vector(GetSplinePointLinear(c3.to_Vector2(),
-    c4.to_Vector2(), 0.50)); Vector coffset_droite_dst =
-    Vector(GetSplinePointLinear(c3.to_Vector2(), c4.to_Vector2(), 0.75)); Vector
-    coffset_gauche_dst = Vector(GetSplinePointLinear(c3.to_Vector2(),
-    c4.to_Vector2(), 0.25));
-
-
-    if (_entry_neighbor % 3 == _exit_neighbor % 3) {
-            // Si l'on doit faire une ligne droite
-            DrawLineEx(coffset_droite_src.to_Vector2(),
-    coffset_droite_dst.to_Vector2(), _width, BLACK);
-            DrawLineEx(coffset_gauche_src.to_Vector2(),
-    coffset_gauche_dst.to_Vector2(), _width, BLACK); } else {
-            // Si il faut calculler un virage
-            //assert(_entry_neighbor == _exit_neighbor + 2);
-            //assert(_entry_neighbor + 2 == _exit_neighbor);
-
-            Hex tile_circle = _hex.neighbor((_entry_neighbor+1) / 2);
-
-            Hex tile_circle2 = _hex.neighbor(3);
-
-            Hex neighbor_src = _hex.neighbor(_entry_neighbor);
-            Hex neighbor_dst = _hex.neighbor(_exit_neighbor);
-
-            //DrawLineEx(neighbor_src.center(layout), _hex.center(layout),
-    _width, BLACK);
-            //DrawLineEx(neighbor_dst.center(layout), _hex.center(layout),
-    _width, BLACK);
-
-            ArcTrack track = ArcTrack{tile_circle.center(layout), 150, 25,
-    _hex.corner_angle(layout, _exit_neighbor-2) * 180 / M_PI,
-    _hex.corner_angle(layout, _entry_neighbor+3) * 180 / M_PI}; track.draw();
-
-            DrawCircleV(tile_circle.center(layout), 4, GREEN);
-            DrawCircleV(tile_circle2.center(layout), 4, RED);
-            DrawCircleV(_hex.center(layout), 4, PURPLE);
-
-
-            Vector2 p1[3];
-
-            Vector offset_droite_src = coffset_droite_src-coffset_center_src;
-            Vector offset_droite_dst = coffset_droite_dst-coffset_center_dst;
-
-            Vector offset_gauche_src = coffset_gauche_src-coffset_center_src;
-            Vector offset_gauche_dst = coffset_gauche_dst-coffset_center_dst;
-
-            DrawLineEx(coffset_droite_src.to_Vector2(),
-    (center+offset_droite_src).to_Vector2(), 2, PURPLE);
-            DrawLineEx(coffset_droite_dst.to_Vector2(),
-    (center+offset_droite_dst).to_Vector2(), 2, PURPLE);
-
-            DrawLineEx(center.to_Vector2(),
-    (center+offset_droite_src).to_Vector2(), 2, GREEN);
-            DrawLineEx(center.to_Vector2(),
-    (center+offset_droite_dst).to_Vector2(), 2, BLUE);
-            DrawLineEx(center.to_Vector2(),
-    (center+offset_droite_src.proj(offset_droite_dst)).to_Vector2(), 2, GREEN);
-            DrawLineEx(center.to_Vector2(),
-    (center+offset_droite_dst.proj(offset_droite_src)).to_Vector2(), 2, BLUE);
-
-            Vector pt_controle_droite = center +
-    ((offset_droite_dst.proj(offset_droite_src) +
-    offset_droite_src.proj(offset_droite_dst)));
-
-            p1[1] = pt_controle_droite.to_Vector2();
-            p1[0] = coffset_droite_src.to_Vector2();
-            p1[2] = coffset_droite_dst.to_Vector2();
-
-            Vector pt_controle_gauche = center +
-    offset_gauche_dst.proj(offset_gauche_src) +
-    offset_gauche_src.proj(offset_gauche_dst); Vector2 p2[3]; p2[1] =
-    pt_controle_gauche.to_Vector2(); p2[0] = coffset_gauche_src.to_Vector2();
-            p2[2] = coffset_gauche_dst.to_Vector2();
-
-
-            DrawSplineBezierQuadratic(p1, 3, _width, BLACK);
-            DrawSplineBezierQuadratic(p2, 3, _width, BLACK);
-            DrawCircleV(p1[1], 4, RED);
-
-    } */
 }
 
 void Rail::draw(Layout *layout, Color c) {
